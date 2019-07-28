18ad5275934cc0da3fc92011836b3d360ca93c96
Lidor <lidor.rosencovich@sisense.com>
2019-07-28 23:52:52 +0300

    home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,14 @@ import {RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {LoginFormComponent} from './login-form/login-form.component';
 import {RegisterFormComponent} from './register-form/register-form.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  // {path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule', canActivate: [AuthGuard]},
-  // {
-  //   path: 'auth',
-  //   component: LoginFormComponent,
-  // },
+  {path: 'home', loadChildren: 'app/home/home.module#HomeModule'},
   {path: ' ', redirectTo: '/login'},
   {path: 'login', component: LoginFormComponent},
-  {path: 'register', component: RegisterFormComponent}
-  // {path: '**', redirectTo: 'auth'}
+  {path: 'register', component: RegisterFormComponent},
+  {path: '**', redirectTo: '/login'}
 ];
 
 @NgModule({
